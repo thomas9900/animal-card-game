03001e60a87dfc5d43ffc6c2f87b0c582544450e
thomas9900 <52283912+thomas9900@users.noreply.github.com>
2020-07-08 20:36:14 +0300

    m

--- app.js
+++ app.js
@@ -1,74 +1,172 @@
-// pseudo code
-// flip card and ask question
-// connect question to animal
-// make right and wrong answers and show
-// show your number of correct and wrong answers
-// show your best gamescore in localstorage
-
-
 let hasFlippedCard = false;
 let lockBoard = false;
-let firstCard;
+let currentCard;
 let correctScore = 0;
 let wrongScore = 0;
 const correctScore_span = document.getElementById('correct-score');
 const wrongScore_span = document.getElementById('wrong-score');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
 const cards = document.querySelectorAll('.animal-card');
-const octo = document.querySelector('.octo');
-const owl = document.querySelector('.owl');
-const elements = {
-    question: document.querySelector('.question'),
-    questionDiv: document.querySelector('.question-div'),
-    firstAnswer: document.querySelector('.a'),
-    secondAnswer: document.querySelector('.b')
-};
 
+const owl = [
+    {
+        question: 'How many owl species are there in the world?',
+        answers: [
+            {text: '225', correct: true},
+            {text: '980', correct: false},
+            {text: '400', correct: false},
+            {text: '1200', correct: false}
+        ]
+    },
+    {
+        question: 'What is the diet of an owl?',
+        answers: [
+            {text: 'Omnivore', correct: false},
+            {text: 'Carnivore', correct: true},
+            {text: 'Herbivore', correct: false},
+            {text: 'Fruitarian', correct: false}
+        ]
+    }
+];
+const snake = [
+    {
+        question: 'How many snake species are there?',
+        answers: [
+            {text: '3000', correct: true},
+            {text: '2000', correct: false},
+            {text: '700', correct: false},
+            {text: '3500', correct: false}
+        ]
+    },
+    {
+        question: 'What is the diet of a snake?',
+        answers: [
+            {text: 'Omnivore', correct: false},
+            {text: 'Carnivore', correct: true},
+            {text: 'Herbivore', correct: false},
+            {text: 'Fruitarian', correct: false}
+        ]
+    }
+];
+const crocodile = [
+    {
+        question: 'How many species of crocodile are there?',
+        answers: [
+            {text: '13', correct: true},
+            {text: '55', correct: false},
+            {text: '70', correct: false},
+            {text: '93', correct: false}
+        ]
+    },
+    {
+        question: 'What is the diet of a crocodile?',
+        answers: [
+            {text: 'Omnivore', correct: false},
+            {text: 'Carnivore', correct: true},
+            {text: 'Herbivore', correct: false},
+            {text: 'Fruitarian', correct: false}
+        ]
+    }
+];
+const octopus = [
+    {
+        question: 'How many species are there of octopus?',
+        answers: [
+            {text: '300', correct: true},
+            {text: '200', correct: false},
+            {text: '90', correct: false},
+            {text: '80', correct: false}
+        ]
+    },
+    {
+        question: "What is the diet of a octopus?",
+        answers: [
+            {text: 'Omnivore', correct: false},
+            {text: 'Carnivore', correct: true},
+            {text: 'Herbivore', correct: false},
+            {text: 'Fruitarian', correct: false}
+        ]
+    }
+];
 
-const flipCard = function() {
-    let animal = this.dataset.framework
-    // console.log(animal)
-    showQuestion(animal);
-    // if (lockBoard === true) return;
-    this.classList.add('flip');
-    // if (this === firstCard) return;
-    
+function flipCard() {
+    let animal = this.dataset.framework;
+
+    if (lockBoard === true) return;
+
     if (hasFlippedCard === false) {
-        //first click
         hasFlippedCard = true;
-        firstCard = this;
-        // return;
-        
+        currentCard = this;
+    }
+
+    this.classList.add('flip');
+    questionContainerElement.classList.remove('hide');
+
+    // remove html answer-buttons
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
     
-    disableCard();
+    // disable current card
+    currentCard.removeEventListener('click', flipCard);
 
+    showQuestion(animal);
 }
 
-// function checkForMatch() {
-//     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+function showQuestion(animal) {
+    lockBoard = true;
+    const randomNum = Math.floor(Math.random() * 2);
+    let question;
 
-//     isMatch ? disableCards() : unflipCards();
-// }
+    // create random question
+    if (animal === 'owl') {
+        question = owl[randomNum];
+    } else if (animal === 'snake') {
+        question = snake[randomNum];
+    } else if (animal === 'crocodile') {
+        question = crocodile[randomNum];
+    } else if (animal === 'octopus') {
+        question = octopus[randomNum];
+    }
+    questionElement.innerText = question.question;
 
-function disableCard() {
-    firstCard.removeEventListener('click', flipCard);
+    // create answer button
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
 
-    // resetBoard(); 
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    });
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    if (correct) {
+        correctScore++;
+        correctScore_span.innerHTML = correctScore;
+        unflipCard();
+    } else 
+    wrongScore++;
+    wrongScore_span.innerHTML = wrongScore;
 }
 
 function unflipCard() {
-    lockBoard = true;
+    currentCard.classList.remove('flip');
+    questionContainerElement.classList.add('hide');
 
-    firstCard.classList.remove('flip');
-    console.log(firstCard)
-    resetBoard();     
-}
-
-function resetBoard() {
+    // reset board  
     [hasFlippedCard, lockBoard] = [false, false];
-    firstCard = null;
 }
 
+// random card position
 (function shuffle() {
     cards.forEach(card => {
         let randomPos = Math.floor(Math.random() * 4);
@@ -77,103 +175,3 @@ function resetBoard() {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
-
-
-const showQuestion = animal => {
-    const questions  = [`
-        <p>Type of a(n) ${animal}?</p>
-        <button class="a">A) Crocodile</button><br>
-        <button class="b">B) Octopus</button><br>
-        <button class="c">C) Owl</button><br>
-        <button class="d">D) Snake</button><br>
-    `,
-    `
-        <p>Color of a(n) ${animal}?</p>
-        <button class="a">A) Green</button><br>
-        <button class="b">B) Red</button><br>
-        <button class="c">C) Blue</button><br>
-        <button class="d">D) Red & Blue</button><br>
-    `]; 
-    const randomQuestion = questions[Math.floor(Math.random() * questions.length)];
-
-
-    elements.questionDiv.insertAdjacentHTML('afterbegin', randomQuestion);
-
-    answerQuestion(animal);
-
-
-}
-
-function answerQuestion(animal) {
-    elements.questionDiv.addEventListener('click', e => {
-        console.log(elements.questionDiv)
-
-        if (e.target.matches('.a') && animal == 'crocodile') {
-            console.log('croc a')
-            correctAnswer();
-        } else if (e.target.matches('.b') && animal === 'octopus') {
-            console.log('octo b')
-            correctAnswer();
-        } else if (e.target.matches('.c') && animal === 'owl') {
-            console.log('owl c')
-            correctAnswer();
-        } else if (e.target.matches('.d') && animal === 'snake') {
-            console.log('snake d')
-            correctAnswer();
-        }
-         else if (e.target.matches('button')) {
-            console.log('wrongscore')
-             wrongAnswer();
-        }
-
-        // console.log(e.target)
-    });
-    // if (animal === 'crocodile') {
-    //     correctAnswer();
-    // }
-    // if (animal === 'octopus') {
-    //     wrongAnswer();
-}
-
-
-// function main() {
-//     elements.firstAnswer.addEventListener('click',() => answerQuestion('crocodile'));
-//     elements.secondAnswer.addEventListener('click',() => answerQuestion('octopus'));
-// }
-
-// main();
-
-
-function correctAnswer() {
-    correctScore++;
-    correctScore_span.innerHTML = correctScore;
-    elements.questionDiv.textContent = '';
-    // elements.questionDiv.parentNode.removeChild(elements.questionDiv);
-
-    // while (elements.questionDiv.firstChild) {
-    //     elements.questionDiv.removeChild(elements.questionDiv.lastChild);
-    // }
-    unflipCard();
-}
-
-function wrongAnswer() {
-    wrongScore++;
-    wrongScore_span.innerHTML = wrongScore;
-}
-
-
-// function game() {
-//     switch () {
-//         case 'rs':
-//         case 'pr':
-//         case 'sp':
-//             correctAnswer();
-//             break;
-//         case 'rp':
-//         case 'ps':
-//         case 'sr':
-//             wrongAnswer();
-//             break;
-//     }
-// }
-
